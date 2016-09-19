9f63fe1690a9cb8c0d46e7f3a6838324ec35de8b
Tomas Hluchan <tomas.hluchan@gmail.com>
2016-09-19 22:32:23 +0200

    cfg, db sync

--- main.js
+++ main.js
@@ -1,53 +1,61 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const ipcMain = electron.ipcMain
+const config = require('electron-json-config');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// CONFIG & IPC 
+console.log("start");
+console.log("userData path: " + app.getPath('userData'));
+console.log("-===========-");
+console.log("config file: " + config.file());
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+console.log(config.all());
 
-  // and load the index.html of the app.
+ipcMain.on('get_config', (event, name) => {
+  console.log("get_config "+name); 
+  event.returnValue = config.get(name); 
+})
+
+ipcMain.on('set_config', (event, name, value) => {
+  console.log("set_config "+name+" "+value); 
+  config.set(name,value);
+  console.log(config.all());
+}) 
+
+ipcMain.on('open_config_dialog', (event) => {
+  console.log("open_config ");
+
+  let child = new BrowserWindow({parent: mainWindow, modal: true, show: true})
+  child.loadURL(`file://${__dirname}/config.html`)
+})
+
+
+
+
+function createMainWindow () {
+  console.log("createMainWindow");
+  mainWindow = new BrowserWindow({width: 800, height: 600, fullscreen: false})
+  
+
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function() {
+   createMainWindow();
+})
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  console.log("on window-all-closed");
+  app.quit();
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,3 +1,103 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+const ipcRenderer = require('electron').ipcRenderer
+
+console.log("render start");
+
+
+//////////////////////////////////////////////////////////////
+// keylogger setup
+function checkKey(e) {
+    e = e || window.event;
+    var t = document.getElementById("keylogger").innerText;
+    if (t.length>70) {
+       t = t.substr(1)
+    }
+    document.getElementById("keylogger").innerText = t +  String.fromCharCode(e.keyCode)
+//    console.log(e.keyCode);
+}
+document.onkeydown = checkKey;
+//////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////
+// config open button
+document.getElementById("btn_open_cfg").onclick = function() {
+	ipcRenderer.send('open_config_dialog')
+}; 
+//////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////
+// open local db
+var PouchDB = require('prcek_pouchdb');
+var local_db = new PouchDB('cdb_local');
+
+function cdb_log(x) {
+    document.getElementById("cdb_status").innerText +=  " " + x;
+}
+
+
+local_db.info().then(function (result){
+   console.log(result);
+   cdb_log("local db = " + result.db_name + " " + result.doc_count + " " + result.update_seq + ";");
+}).catch(function (err){
+   cdb_log("local db err = " + err);
+   console.log(err);
+});
+
+
+var remote_db_url = ipcRenderer.sendSync('get_config', 'cdb_url');
+var remote_db = new PouchDB(remote_db_url);
+
+
+remote_db.info().then(function (result){
+   console.log(result);
+   cdb_log("remote db = " + result.db_name + " " + result.doc_count + " " + result.update_seq + ";");
+}).catch(function (err){
+   cdb_log("remote db err = " + err);
+   console.log(err);
+});
+
+
+let replication;
+function cdb_sync() {
+   replication = PouchDB.replicate(remote_db,local_db, {live:false, retry:true}).on('change', function (info) {
+   cdb_log('change ' + info);
+}).on('paused', function (err) {
+  // replication paused (e.g. replication up to date, user went offline)
+   cdb_log('paused '+err);
+}).on('active', function () {
+  // replicate resumed (e.g. new changes replicating, user went back online)
+   cdb_log('active');
+}).on('denied', function (err) {
+  // a document failed to replicate (e.g. due to permissions)
+   cdb_log('denied ' +err);
+}).on('complete', function (info) {
+  // handle complete
+   cdb_log('complete '+info);
+   console.log(info);
+}).on('error', function (err) {
+  // handle error
+   cdb_log('error '+err);
+}); 
+}
+
+document.getElementById("btn_cdb_sync").onclick = function() {
+	cdb_log("sync start");
+	cdb_sync();	
+}; 
+
+document.getElementById("btn_cdb_sync_cancel").onclick = function() {
+	cdb_log("sync cancel");
+        replication.cancel();	
+}; 
+
+
+
+
+
